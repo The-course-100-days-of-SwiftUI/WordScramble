a9abfac919a27ed721b584f3f3a01a8306015e0a
Margarita <margaritamayer55@gmail.com>
2024-03-10 23:23:17 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Margarita Mayer on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
